5c6bc7c1bc1aefe507721aaa210f44708e9ae303
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2024-09-23 19:41:21 +0800

    no message

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -11,35 +11,40 @@ import { Tag } from "antd";
 
 const Pricing = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
-let  financial=[{
-  title:"建设银行-抵押快贷",
-  type:'不限',
-  rateDown:'3.10',
-  rateUp:'3.90',
-  term:36,
-  amount:1000
-},{
-  title:"建设银行-抵押快贷",
-  type:'不限',
-  rateDown:'3.10',
-  rateUp:'3.90',
-  term:36,
-  amount:1000
-},{
-  title:"建设银行-抵押快贷",
-  type:'不限',
-  rateDown:'3.10',
-  rateUp:'3.90',
-  term:36,
-  amount:1000
-},{
-  title:"建设银行-抵押快贷",
-  type:'不限',
-  rateDown:'3.10',
-  rateUp:'3.90',
-  term:36,
-  amount:1000
-}]
+  let financial = [
+    {
+      title: "建设银行-抵押快贷",
+      type: "不限",
+      rateDown: "3.10",
+      rateUp: "3.90",
+      term: 36,
+      amount: 1000,
+    },
+    {
+      title: "建设银行-抵押快贷",
+      type: "不限",
+      rateDown: "3.10",
+      rateUp: "3.90",
+      term: 36,
+      amount: 1000,
+    },
+    {
+      title: "建设银行-抵押快贷",
+      type: "不限",
+      rateDown: "3.10",
+      rateUp: "3.90",
+      term: 36,
+      amount: 1000,
+    },
+    {
+      title: "建设银行-抵押快贷",
+      type: "不限",
+      rateDown: "3.10",
+      rateUp: "3.90",
+      term: 36,
+      amount: 1000,
+    },
+  ];
   return (
     <div
       className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
@@ -52,63 +57,72 @@ let  financial=[{
               variants={scrollAnimation}
               className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed"
             >
-             金融产品
+              金融产品
             </motion.h3>
             <motion.p
               variants={scrollAnimation}
               className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
             >
-             让我们选择最适合您的套餐，并愉快地探索它。
+              让我们选择最适合您的套餐，并愉快地探索它。
             </motion.p>
           </ScrollAnimationWrapper>
           <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-4 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
-            {financial.map(item=>{
-              let {title,type}=item
-              return <ScrollAnimationWrapper className="flex justify-center">
-              <motion.div
-                variants={scrollAnimation}
-                className="flex flex-col border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-10"
-                whileHover={{
-                  scale : 1.1,
-                  transition: {
-                    duration: .2
-                  }
-                }}
-              >
-                <div className="p-4 lg:p-0 mt-6 lg:mt-10 flex items-end">
-                  <img
-                    src="/assets/Free.png" 
-                    className="h-[40px] mr-2"
-                  />建设银行
-                </div>
-                <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
-                 {title}
-                </p>
-                <Tag>{type}</Tag>
-              
-                <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
-                  <ButtonOutline>查看详情</ButtonOutline>
-                </div>
-              </motion.div>
-            </ScrollAnimationWrapper>
+            {financial.map((item, index) => {
+              let { title, type } = item;
+              return (
+                <ScrollAnimationWrapper
+                  className="flex justify-center"
+                  key={index}
+                >
+                  <motion.div
+                    variants={scrollAnimation}
+                    className="flex flex-col border-2 border-gray-500 rounded-xl py-4 px-6 lg:px-12 xl:px-10"
+                    whileHover={{
+                      scale: 1.1,
+                      transition: {
+                        duration: 0.2,
+                      },
+                    }}
+                  >
+                    <div className="p-4 lg:p-0 mt-6 lg:mt-10 flex items-end">
+                      <img src="/assets/Free.png" className="h-[40px] mr-2" />
+                      建设银行
+                    </div>
+                    <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
+                      {title}
+                    </p>
+                    <Tag>{type}</Tag>
+
+                    <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
+                      <ButtonOutline>查看详情</ButtonOutline>
+                    </div>
+                  </motion.div>
+                </ScrollAnimationWrapper>
+              );
             })}
-           
           </div>
         </div>
         <div className="flex flex-col w-full my-16">
           <ScrollAnimationWrapper>
             <motion.h3
               variants={scrollAnimation}
-              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed w-9/12 sm:w-6/12 lg:w-4/12 mx-auto">
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-relaxed w-9/12 sm:w-6/12 lg:w-4/12 mx-auto"
+            >
               Huge Global Network of Fast VPN{" "}
             </motion.h3>
-            <motion.p className="leading-normal  mx-auto my-2 w-10/12 sm:w-7/12 lg:w-6/12" variants={scrollAnimation}>
+            <motion.p
+              className="leading-normal  mx-auto my-2 w-10/12 sm:w-7/12 lg:w-6/12"
+              variants={scrollAnimation}
+            >
               See LaslesVPN everywhere to make it easier for you when you move
               locations.
             </motion.p>
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper>
-            <motion.div className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap" variants={scrollAnimation}>
+            <motion.div
+              className="w-full flex justify-evenly items-center mt-4 flex-wrap lg:flex-nowrap"
+              variants={scrollAnimation}
+            >
               {/* <Netflix className="h-18 w-auto" /> */}
               <img
                 src="/assets/Icon/amazon.png"
@@ -142,15 +156,16 @@ let  financial=[{
           <ScrollAnimationWrapper>
             <motion.h3
               variants={scrollAnimation}
-              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto"
+            >
               Trusted by Thousands of Happy Customer{" "}
             </motion.h3>
             <motion.p
               variants={scrollAnimation}
               className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
             >
-              These are the stories of our customers who have joined us with great
-              pleasure when using this crazy feature.
+              These are the stories of our customers who have joined us with
+              great pleasure when using this crazy feature.
             </motion.p>
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="w-full flex flex-col py-12">
@@ -159,7 +174,7 @@ let  financial=[{
             </motion.div>
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="relative w-full mt-16">
-            <motion.div variants={scrollAnimation} custom={{duration: 3}}>
+            <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
               <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
                 <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
                   <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
@@ -172,7 +187,7 @@ let  financial=[{
               <div
                 className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
                 style={{ filter: "blur(114px)" }}
-                ></div>
+              ></div>
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
