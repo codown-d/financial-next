d78b4b025b167c819e0584ce245894ff1ff34278
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-10-10 22:40:22 +0800

    no message

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,17 @@
+const os = require('os');
+
+// 获取本机的内网 IP 地址
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (let interfaceName in interfaces) {
+    for (let net of interfaces[interfaceName]) {
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address; // 返回第一个找到的内网 IP
+      }
+    }
+  }
+}
+const localIP = getLocalIP();
 module.exports = {
     apps: [
       {
@@ -7,6 +21,7 @@ module.exports = {
         env: {
           NODE_ENV: 'production',
           PORT: 3000,
+          HOST: localIP, // 使用动态获取的 IP 地址
           // 其他环境变量
         },
       },
