59e8485e515596d6eecc9c66ce455765fe775612
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-11-01 02:24:53 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,14 @@
 # 拉取最新代码（如果使用 Git）
 git pull origin main
 
-pm2 stop next-app
+pm2 delete all
+
 # 构建项目
 npm run build
 
 # 启动 PM2 应用
 pm2 start npm --name "next-app" -- start
 
-pm2 restart next-app
 # 查看应用状态
 pm2 list
 
-# 可选：保存 PM2 状态
-pm2 save
