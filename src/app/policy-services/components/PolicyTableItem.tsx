63030c3617aa273a51452fe69c4697dc4bad8968
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2024-10-31 10:10:48 +0800

    no message

--- src/app/policy-services/components/PolicyTableItem.tsx
+++ src/app/policy-services/components/PolicyTableItem.tsx
@@ -5,25 +5,26 @@ import TzIcon from "../../../components/TzIcon";
 import { DataType } from "@/app/policy-services/components/ClientTable";
 
 export default function PolicyTableItem(props: DataType) {
-  let { title, fwbh, publishdate, id ,rowguid} = props;
+  let { title, fwbh, publishdate, id, rowguid } = props;
   const router = useRouter();
   return (
     <div>
       <div className="font-medium text-[20px] mb-5 text-[#333333] leading-[20px]">
-        {fwbh}
+        {title}
       </div>
-      <div className="text-[#999] text-xs mb-5">{title}</div>
+      <div className="text-[#999] text-xs mb-5">{fwbh}</div>
       <div className="flex items-center justify-between">
         {/* <span>{timeFormat(time)}</span> */}
         <span>{publishdate}</span>
         <TzButton
           type="link"
-          size={'small'}
+          size={"small"}
           className="!text-[#3D5AF5]"
-          icon={<TzIcon className={"fa-chevron-right text-[#3D5AF5] text-sm"} />}
+          icon={
+            <TzIcon className={"fa-chevron-right text-[#3D5AF5] text-sm"} />
+          }
           iconPosition={"end"}
-          onClick={()=>{
-            
+          onClick={() => {
             router.push(`/policy-detail?policyguid=${rowguid}`);
           }}
         >
--- src/components/UI/FundContent.tsx
+++ src/components/UI/FundContent.tsx
@@ -5,13 +5,13 @@ export default function FundContent() {
   return (
     <>
       <div className="text-2xl font-bold mb-4 flex justify-center mb-10">
-        基金申请
+        业务申请
       </div>
       <div className="flex justify-between items-center px-[46px]">
         <div className="">
           <div className="mb-8 text-[20px] leading-[20px]">请您联系：</div>
           <div className="text-[#3D5AF5] text-[20px] font-bold ">
-          0839-3271399
+            0839-3271399
           </div>
           <div className="text-[#999]">工作日：09:00-18:00 在线</div>
         </div>
