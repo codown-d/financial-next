5410483aafbf79869cbf8a83a802e6129128ddb2
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-11-04 01:34:32 +0800

    no message

--- src/app/personal-center/layout.tsx
+++ src/app/personal-center/layout.tsx
@@ -3,6 +3,7 @@ import TzCard from "@/components/TzCard";
 import TzTag from "@/components/TzCheckableTag";
 import TzIcon from "@/components/TzIcon";
 import TzNextImage from "@/components/TzNextImage";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -26,6 +27,7 @@ export default function PersonalCenterLayout(props) {
     setActiveItem(lastSegment);
   }, [pathname]);
   return (
+    <AntdRegistry>
     <div className="bg-[#F8F8F8] h-full pt-3 pb-[90px]">
       <div className="flex-r-c max-w-screen-lg !items-start  mx-auto">
         <TzCard className="w-[240px]" styles={{ body: { padding: "14px" } }}>
@@ -85,5 +87,6 @@ export default function PersonalCenterLayout(props) {
         <div className="flex-1 w-0 ml-3">{children}</div>
       </div>
     </div>
+    </AntdRegistry>
   );
 }
